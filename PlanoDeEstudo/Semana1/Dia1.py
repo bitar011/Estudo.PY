932bcfe2cd25eb3ec605f07de77f78d6e19d163a
bitar011 <victorbitarr@gmail.com>
2025-09-30 19:13:07 -0300

    Práticas básicas de python, a respeito de variáveis, operadores e sintaxe

---
+++ PlanoDeEstudo/Semana1/Dia1.py
@@ -0,0 +1,31 @@
+#Estudo de variáveis
+#Dia 1
+# nome = input("Digite seu nome: ")
+# idade = int(input("Agora, digite sua idade: "))
+
+# print(f"Olá {nome}, você tem {idade} anos")
+
+# nome = "Victor"
+# idade = 19
+# altura = 1.78
+
+# nome = "Ana"
+# idade = 17
+# idade = "dezessete"
+# altura = 1.65
+
+# print(nome, idade, altura)
+
+# pi = 3.14
+# raio = 5
+
+# area = pi * raio**2
+# print(area)
+
+# list = [1, 2, 3, 4, 5]
+
+# user = int(input("Digite: "))
+# if user in list:
+#     print("Está!")
+# else:
+#     print("Não está")
