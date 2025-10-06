feedeffb0d8a2d72bddecfd145d2d0358aeaa77c
bitar011 <victorbitarr@gmail.com>
2025-10-06 19:21:52 -0300

    correção na condição de recupração

--- PlanoDeEstudo/Semana 2/Dia 1/verificaçãoDeAprovação.py
+++ PlanoDeEstudo/Semana 2/Dia 1/verificaçãoDeAprovação.py
@@ -2,7 +2,7 @@ nota = float(input("Digite sua nota: "))
 
 if nota < 5:
     print("Reprovado")
-elif nota < 7 and nota > 5:
+elif nota < 7 and nota >= 5:
     print("Recuperação")
 else:
     print("Aprovado!")
